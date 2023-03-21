404d11a0d7ee5b6854358ed80cc0c9638c4ff74a
Muhammad Owais Ahmed <110997551+osie404@users.noreply.github.com>
2023-03-21 12:09:41 +0500

    server.sql

---
+++ .github/server.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE [APItest]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'APItest', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER2019\MSSQL\DATA\APItest.mdf' , SIZE = 8192KB , FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'APItest_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER2019\MSSQL\DATA\APItest_log.ldf' , SIZE = 8192KB , FILEGROWTH = 65536KB )
+GO
+ALTER DATABASE [APItest] SET COMPATIBILITY_LEVEL = 150
+GO
+
+USE APItest;
+GO
+
+CREATE LOGIN 123  
+    WITH PASSWORD = '123';  
+GO  
+
+-- Creates a database user for the login created above.  
+CREATE USER 123 FOR LOGIN 123;  
+GO  
+
+CREATE USER [123] FROM LOGIN [sntk\123];
+GO
+
+USE APITest;
+
+CREATE TABLE dbo.UsersAD
+( ID INT IDENTITY(1,1) NOT NULL
+,EmloyeeID INT 
+,AccountName VARCHAR(100)
+,DisplayName VARCHAR(200)
+,Email VARCHAR(100)
+)
+
+
+INSERT INTO dbo.UsersAD (EmloyeeID, SamAccountName, DisplayName, Email)
+          
+DROP TABLE IF EXISTS dbo.usersAD2
+CREATE TABLE dbo.usersAD2 
+(id INT IDENTITY(1,1) NOT NULL
+,dateinserted DATETIME DEFAULT(getdate()) NOT NULL
+,val INT
+,val2 INT)
+
+
+DROP TABLE IF EXISTS dbo.TestChar
+CREATE TABLE dbo.TestChar
+(id INT IDENTITY(1,1) NOT NULL
+,tekst VARCHAR(100)
+,ts DATETIME DEFAULT(getdate()) NOT NULL)
